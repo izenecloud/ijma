16f4c0f7c1fa03ef4a532916255bfdd38efc3024
Vernkin Chen <vernkin@gmail.com>
2009-06-18 11:09:22 +0800

    add jma_analyzer

---
+++ source/include/jma_analyzer.h
@@ -0,0 +1,90 @@
+/** \file crf_analyzer.h
+ * Definition of class CRF_Analyzer.
+ *
+ * \author Jun Jiang
+ * \version 0.1
+ * \date Mar 2, 2009
+ */
+
+#ifndef JMA_CRF_ANALYZER_H
+#define JMA_CRF_ANALYZER_H
+
+#include "analyzer.h"
+#include "sentence.h"
+#include "mecab.h"
+
+using namespace std;
+
+namespace jma
+{
+
+class JMA_Knowledge;
+
+/**
+ * JMA_Analyzer executes the Japanese morphological analysis based on conditional random field.
+ */
+class JMA_Analyzer : public Analyzer
+{
+public:
+    /**
+     * Constructor.
+     */
+    JMA_Analyzer();
+
+    /**
+     * Destructor.
+     */
+    ~JMA_Analyzer();
+
+    /**
+     * Set the \e Knowledge for analysis.
+     * \param pKnowledge the pointer of \e Knowledge
+     */
+    virtual void setKnowledge(Knowledge* pKnowledge);
+
+    /**
+     * Execute the morphological analysis based on a sentence.
+     * \param sentence the instance containing the raw sentence string and also to save the analysis result
+     * \return 0 for fail, 1 for success
+     * \pre the raw sentence string could be got by \e sentence.getString().
+     * \post on successful return, the n-best results could be got by \e sentence.getListSize(), etc.
+     * \attention white-space characters are ignored in the analysis, which include " \t\n\v\f\r", and also space character in specific encoding.
+     */
+    virtual int runWithSentence(Sentence& sentence);
+
+    /**
+     * Execute the morphological analysis based on a paragraph string.
+     * \param inStr paragraph string
+     * \return 0 for fail, otherwise a non-zero string pointer for the one-best result
+     */
+    virtual const char* runWithString(const char* inStr);
+
+    /**
+     * Execute the morphological analysis based on a file.
+     * \param inFileName input file name
+     * \param outFileName output file name
+     * \return 0 for fail, 1 for success
+     * \post on successful return, the one-best result is printed to file \e outFileName.
+     */
+    virtual int runWithStream(const char* inFileName, const char* outFileName);
+
+    /**
+     * Split a paragraph string into sentences.
+     * \param paragraph paragraph string
+     * \param sentences sentence vector
+     * \attention the original elements in \e sentences would not be removed, and the splited sentences are appended into \e sentences.
+     */
+    virtual void splitSentence(const char* paragraph, std::vector<Sentence>& sentences);
+
+private:
+	void analyzerSentence(const char *str, vector<const MeCab::Node*>& nodes,
+			vector<double>* scores, int N);
+
+private:
+	JMA_Knowledge* knowledge_;
+	MeCab::Tagger* tagger_;
+};
+
+} // namespace jma
+
+#endif // JMA_CRF_Analyzer_H
---
+++ source/src/jma_analyzer.cpp
@@ -0,0 +1,144 @@
+/** \file crf_analyzer.cpp
+ * Implementation of class CRF_Analyzer.
+ *
+ * \author Jun Jiang
+ * \version 0.1
+ * \date Mar 2, 2009
+ */
+
+
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include "jma_analyzer.h"
+#include "jma_knowledge.h"
+#include "mecab.h"
+
+using namespace std;
+
+namespace jma
+{
+
+JMA_Analyzer::JMA_Analyzer()
+    : knowledge_(0), tagger_(0)
+{
+}
+
+JMA_Analyzer::~JMA_Analyzer()
+{
+
+}
+
+void JMA_Analyzer::setKnowledge(Knowledge* pKnowledge)
+{
+    assert(pKnowledge);
+
+    knowledge_ = dynamic_cast<JMA_Knowledge*>(pKnowledge);
+    assert(knowledge_);
+    tagger_ = knowledge_->getTagger();
+    assert(tagger_);
+}
+
+int JMA_Analyzer::runWithSentence(Sentence& sentence)
+{
+	int N = (int)getOption(Analyzer::OPTION_TYPE_NBEST);
+	return 1;
+}
+
+const char* JMA_Analyzer::runWithString(const char* inStr)
+{
+    return 0;
+}
+
+int JMA_Analyzer::runWithStream(const char* inFileName, const char* outFileName)
+{
+
+    return 1;
+}
+
+void JMA_Analyzer::splitSentence(const char* paragraph, std::vector<Sentence>& sentences)
+{
+    /*if(! paragraph)
+        return;
+
+    Sentence result;
+    string sentenceStr;
+    Tokenizer tokenizer(*ctype_);
+    tokenizer.assign(paragraph);
+    for(const char* p=tokenizer.next(); p; p=tokenizer.next())
+    {
+        if(ctype_->isSentenceSeparator(p))
+        {
+            sentenceStr += p;
+
+            result.setString(sentenceStr.c_str());
+            sentences.push_back(result);
+
+            sentenceStr.clear();
+        }
+        // white-space characters are also used as sentence separator,
+        // but they are ignored in the sentence result
+        else if(ctype_->isSpace(p))
+        {
+            if(! sentenceStr.empty())
+            {
+                result.setString(sentenceStr.c_str());
+                sentences.push_back(result);
+
+                sentenceStr.clear();
+            }
+        }
+        else
+        {
+            sentenceStr += p;
+        }
+    }
+
+    // in case the last character is not space or sentence separator
+    if(! sentenceStr.empty())
+    {
+        result.setString(sentenceStr.c_str());
+        sentences.push_back(result);
+
+        sentenceStr.clear();
+    }*/
+}
+
+void JMA_Analyzer::analyzerSentence(const char *str,
+		vector<const MeCab::Node*>& nodes, vector<double>* scores, int N){
+	//one best
+	if(N <= 1){
+		const MeCab::Node* ret = tagger_->parseToNode(str);
+		nodes.push_back(ret);
+		//don't update scores vector here
+		return;
+	}
+
+	assert(scores);
+
+	//N-best
+	vector<long> lgScore; //score with long type
+	long lgTotalScore = 0; // total of score with long type
+
+	//TODO check return value of parseNBestInit here
+	tagger_->parseNBestInit(str);
+	int i = 0;
+	for ( ; i < N; ++i) {
+		const MeCab::Node* ret = tagger_->nextNode();
+		if(!ret)
+			break;
+		nodes.push_back(ret);
+		long score = tagger_->nextScore();
+		lgTotalScore += score;
+		lgScore.push_back(score);
+	}
+
+	//update the N-best score
+	for( int j=0; j<i; ++j ){
+		scores->push_back(lgScore[j] * 1.0 / lgTotalScore);
+	}
+}
+
+} // namespace jma
